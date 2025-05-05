52459b9d6caa22a802de968b9557dfc436270fde
Kevin <kzhe0012@student.monash.edu>
2025-05-05 23:22:24 +1000

    Initial commit

---
+++ smart-service/index.ts
@@ -0,0 +1,47 @@
+import { initTRPC } from "@trpc/server";
+import { applyWSSHandler } from "@trpc/server/adapters/ws";
+import { run } from "smart";
+import { WebSocketServer } from "ws";
+import { z } from "zod";
+
+const t = initTRPC.create();
+
+export const appRouter = t.router({
+  run: t.procedure
+    .input(
+      z.object({
+        agents: z.number().min(1, "Agent count must be at least 1"),
+        scen: z.string().nonempty("Scen file must not be empty"),
+        map: z.string().nonempty("Map file must not be empty"),
+        paths: z.string().nonempty("Paths file must not be empty"),
+      })
+    )
+    .subscription(async function* (opts) {
+      const { input } = opts;
+      const { values, dispose } = await run(input);
+      yield* values();
+      await dispose();
+    }),
+});
+
+export type AppRouter = typeof appRouter;
+
+const wss = new WebSocketServer({ port: 8194 });
+
+const handler = applyWSSHandler({
+  wss,
+  router: appRouter,
+  // Enable heartbeat messages to keep connection open (disabled by default)
+  keepAlive: {
+    enabled: true,
+    // server ping message interval in milliseconds
+    pingMs: 30000,
+    // connection is terminated if pong message is not received in this many milliseconds
+    pongWaitMs: 5000,
+  },
+});
+
+process.on("SIGTERM", () => {
+  handler.broadcastReconnectNotification();
+  wss.close();
+});
