b843cc48add79d0d6a5a2687553b700316b6e1c2
ZhifeiLi-cpu <zhifeil@andrew.cmu.edu>
2025-05-23 09:01:45 -0400

    Update submodule commits for smart and smart-visualiser; enhance error handling and tracking in appRouter

--- smart-service/index.ts
+++ smart-service/index.ts
@@ -1,8 +1,22 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, tracked, type TrackedEnvelope } from "@trpc/server";
 import { applyWSSHandler } from "@trpc/server/adapters/ws";
 import { run, type Output } from "smart";
 import { WebSocketServer } from "ws";
 import { z } from "zod";
+
+const tracker = <T extends object>() => {
+  let i = 0;
+  return (async function* (t: T | AsyncIterableIterator<T>) {
+    if (Symbol.asyncIterator in t) {
+      for await (const t1 of t) {
+        yield tracked(`${i++}`, t1);
+      }
+    }
+    else {
+      yield tracked(`${i++}`, t)
+    }
+  })
+}
 
 const t = initTRPC.create();
 
@@ -14,16 +28,23 @@ export const appRouter = t.router({
         scen: z.string().nonempty("Scen file must not be empty"),
         map: z.string().nonempty("Map file must not be empty"),
         paths: z.string().nonempty("Paths file must not be empty"),
+        lastEventId: z.string().nullish(),
       })
     )
-    .subscription(async function* (opts): AsyncGenerator<Output[]> {
+    .subscription(async function* (opts): AsyncGenerator<TrackedEnvelope<Output[]>> {
+      const track = tracker<Output[]>();
       const { input } = opts;
+      if (input.lastEventId) {
+        // Disconnect  
+        yield* track([{ type: "message", content: "Request cancelled: disconnected" }]);
+        return;
+      }
       const { values, dispose, errors } = await run(input);
       try {
-        yield* values();
-        yield [{ type: "message", content: await errors }];
+        yield* track(values());
+        yield* track([{ type: "message", content: await errors }]);
       } catch (e) {
-        yield [{ type: "error", error: e }];
+        yield* track([{ type: "error", error: e }]);
       } finally {
         await dispose();
       }
